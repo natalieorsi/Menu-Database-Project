9a4356e34be8dff94a991720008e3238bd1854ed
Norsi <natalieannorsi@gmail.com>
2016-11-13 23:11:28 -0800

    general cleanup / visuals
    also imported os

--- database_setup.py
+++ database_setup.py
@@ -1,39 +1,37 @@
 #######Configuration##########
+import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+
 Base = declarative_base()
 
-######Class##########
+##Class##
 #representation of table as a python class
 #extends the Base class
 #nested inside will be table and mapper code
 #camel-cased
 class Restaurant(Base):
-	#####Table#####
+	##Table##
 	#syntax: __tablename__ = 'table_name'
 	__tablename__ = 'restaurant'
-	#####Mapper#####
+	##Mapper##
 	name = Column(String(80), nullable = False) #String type w max length 80, never Null
 	id = Column(Integer, primary_key = True) #another Column, this time int 
 
 class Menu(Base):
-	####Table#####
+	##Table##
 	__tablename__ = 'menu_item'
-	####Mapper####
+	##Mapper##
 	name = Column(String(80),nullable=False)
 	id = Column(String(250))
 
 	course = Column(String(250))
-
 	description = Column(String(250))
-
 	price = Column(String(8))
-
 	restaurant_id = Column(Integer, ForeignKey('restaurant.id')) #link it to the Restaurant Table's foreign ID key
-
 	restaurant = relationship(Restaurant)
 		
 
